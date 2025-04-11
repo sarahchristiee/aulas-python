2cc22fa96b61e68f3b736bfdb9cc96d8444da5b9
Sarah Christie <sarahchristiearaujo@gmail.com>
2025-04-11 13:43:39 -0300

    Update exercicio3.py

--- exercicio3.py
+++ exercicio3.py
@@ -1,5 +1,49 @@
-escolha = int(input("Escolha uma opção para saber mais sobres os príncipais deuses do Olimpo:\n"
-                    "1- Zeus | 2- Hera | 3- Poseidon\n"
-                    "4- Atena 5- Ares 6- Deméter\n"
-                    "7- Apolo 8- Ártemis 9- Hefesto\n"
-                    "10- Afrodite 11- Hermes 12- Dionísio\n"))
+while True:
+
+    escolha = int(input("Escolha uma opção para saber mais sobres os príncipais deuses do Olimpo:\n"
+                        "1- Zeus | 2- Hera | 3- Poseidon\n"
+                        "4- Atena 5- Ares 6- Deméter\n"
+                        "7- Apolo 8- Ártemis 9- Hefesto\n"
+                        "10- Afrodite 11- Hermes 12- Dionísio\n"
+                        "13 - sair\n"
+                        "Digite: "))
+
+    if escolha == 1:
+        print("\n\n\n\nDeus supremo do Olimpo, senhor do céu e do trovão.\nGovernante dos deuses.\nCasado com Hera, mas age como se fosse solteiro.\n🧐 Curiosidade: Seus símbolos são o raio e a águia.\n\n\n\n")
+
+    elif escolha == 2:
+        print("\n\n\n\nRainha dos deuses, deusa do casamento, família, fertilidade e maternidade.\nProtetora das mulheres grávidas e em trabalho de parto.\n🧐 Curiosidade: Apesar de ser esposa de Zeus, vive às voltas com suas infidelidades.\n\n\n\n")
+
+    elif escolha == 3:
+        print("\n\n\n\nDeus dos mares, das tempestades e dos terremotos.\nIrmão de Zeus e um dos deuses mais temidos.\n🧐 Curiosidade: Criou o primeiro cavalo a partir da espuma do mar.\n\n\n\n")
+
+    elif escolha == 4:
+        print("\n\n\n\nAtena é a deusa grega da sabedoria, da inteligência, da justiça, das artes, da guerra, da estratégia e da civilização.\nEla era também a protetora de vários grupos sociais, como arquitetos, tecelões, poetas, filósofos e heróis.\n 🧐 Curiosidade: Nasceu da cabeça de Zeus, já adulta e armada.\n\n\n\n")
+
+    elif escolha == 5:
+        print("\n\n\n\nDeus da guerra e da violência. Representa o lado brutal e caótico dos conflitos.\nEle é filho de Zeus e Hera.\n🧐 Curiosidade: Apesar de sua força, era impopular até entre os próprios deuses.\n\n\n\n\n")
+
+    elif escolha == 6:
+        print("\n\n\n\nDeméter é a deusa grega da agricultura, colheita, fertilidade, estações do ano e terra cultivada.\nEla é também conhecida como a Mãe Terra e a Mãe dos Grãos.\n🧐 Curiosidade: As estações do ano surgiram da dor pela perda de sua filha Perséfone.\n\n\n\n")
+
+    elif escolha == 7:
+        print("\n\n\n\nApolo era o deus grego do Sol, da música, da poesia, das artes, da medicina, da profecia, da verdade, da ordem e da justiça.\nEle era também conhecido como o deus da cura e da proteção.\nÉ irmão gêmeo de Ártemis e filho de Zeus e Leto.\n\n\n\n")
+
+    elif escolha == 8:
+        print("\n\n\n\nNa mitologia grega, Ártemis era a deusa da caça, da vida selvagem, dos animais, do parto, da castidade, da lua e da natureza. Ela era também a protetora das mulheres, das crianças e dos nascimentos. É irmã gêmea de Apolo e filha de Zeus e Leto.\n🧐 Curiosidade: Escolheu viver em castidade e liderava um grupo de ninfas caçadoras.\n\n\n\n")
+
+    elif escolha == 9:
+        print("\n\n\n\nHefesto é o deus grego do fogo, da metalurgia, dos vulcões, dos artesãos, dos ferreiros e dos escultores. Ele é filho de Zeus e Hera. \n\n\n\n")
+
+    elif escolha == 10:
+        print("\n\n\n\nAfrodite é a deusa grega do amor, da beleza, da sexualidade, da fertilidade e do desejo. Ela era uma figura vaidosa e sedutora, que personificava o padrão de beleza da Antiguidade grega. Filha de Zeus e Dione. Foi casada com Hefesto porém ela e Ares foram amantes.\n\n\n\n")
+
+    elif escolha == 11:
+        print("\n\n\n\nHermes é o deus grego do comércio, das viagens, da riqueza, da sorte, dos ladrões, da diplomacia, entre outros. Ele também é conhecido como o mensageiro dos deuses. Filho de Zeus e da ninfa Maia.\n\n\n\n")
+
+    elif escolha == 12:
+        print("\n\n\n\nDionísio é o deus grego do vinho, das festas, do teatro, da fertilidade, da natureza e dos ciclos vitais. Ele também era conhecido como o deus do êxtase e da insanidade. Filho de Zeus e Sêmele\n\n\n\n")
+
+    elif escolha == 13:
+        print("\n\nSistema encerrado!")
+        break
